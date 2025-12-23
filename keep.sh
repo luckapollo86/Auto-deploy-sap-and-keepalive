8f0ec5b81d10d3523cba0a95493a02a841121402
luckapollo86 <151613608+luckapollo86@users.noreply.github.com>
2025-12-23 09:44:03 +0800

    Update email, password, and URLs in keep.sh

--- keep.sh
+++ keep.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 环境变量配置
-EMAIL="xxxxx@xxxx.com"   # 登录邮箱
-PASSWORD="xxxxxxxxx"     # 登录密码
+EMAIL="shangfeng147@gmail.com"   # 登录邮箱
+PASSWORD="@wengwcn0605"     # 登录密码
 
 # 要保活的URL列表,多个用英文空格分隔
-URLS="https://xxxx.cfapps.ap21.hana.ondemand.com https://xxxx.cfapps.us10-001.hana.ondemand.com"  
+URLS="https://aws-usvadkwspg.cfapps.us10-001.hana.ondemand.com https://moontv-awsusvaizjvyb.cfapps.us10-001.hana.ondemand.com"  
 
 # 颜色定义
 green() {
